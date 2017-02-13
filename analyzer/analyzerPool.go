c7252584ddf708a92832ee716934dd81949f1603
yanchenxu <yanchenxu1993@163.com>
2017-02-13 14:06:27 +0800

    analyzerPool

---
+++ analyzer/analyzerPool.go
@@ -0,0 +1,53 @@
+package analyzer
+
+import (
+	"github.com/yanchenxu/Web-spider/middleware"
+	"reflect"
+	"fmt"
+)
+
+type myAnalyzerPool struct {
+	pool  middleware.Pool
+	etype reflect.Type
+}
+
+type genAnalyzer func() Analyzer
+
+func NewAnalyzerPool(total uint32, gen genAnalyzer) (AnalyzerPool, error) {
+	etype := reflect.TypeOf(gen())
+
+	genEntity := func() middleware.Entity {
+		return gen()
+	}
+	pool, err := middleware.NewPool(total, etype, genEntity)
+	if err != nil {
+		return nil, err
+	}
+
+	return &myAnalyzerPool{pool:pool, etype:etype}, nil
+}
+
+func (aPool *myAnalyzerPool)Take() (Analyzer, error) {
+	entity, err := aPool.pool.Take()
+	if err != nil {
+		return nil, err
+	}
+
+	analyzer, ok := entity.(Analyzer)
+	if !ok {
+		panic(fmt.Errorf("The type of entity is NOT %s!\n", aPool.etype))
+	}
+	return analyzer, nil
+}
+
+func (aPool *myAnalyzerPool)Return(ananlyzer Analyzer) error {
+	return aPool.pool.Return(ananlyzer)
+}
+
+func (aPool *myAnalyzerPool)Total() uint32 {
+	return aPool.pool.Total()
+}
+
+func (aPool *myAnalyzerPool)Used() uint32 {
+	return aPool.pool.Used()
+}
